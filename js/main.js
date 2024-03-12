0e80cb9de1975df015728b1f8e23e50f15da36f9
Patricia Atanasoff <system.informatica365@gmail.com>
2024-03-11 23:07:27 -0300

    dark neumorphism, text color depending on the result

--- js/main.js
+++ js/main.js
@@ -1,24 +1,38 @@
-function calculateIMC(){
+function calculateIMC() {
     let weight = document.getElementById('weight').value
     let height = document.getElementById('height').value
 
 
-    if(weight !== '' && height !== ''){
-        let bmi = weight / ((height / 100) **2)
+    if (weight !== '' && height !== '') {
+        let bmi = weight / ((height / 100) ** 2)
         let result = document.getElementById('result')
 
-        result.innerHTML = 'Tu IMC es: ' + bmi.toFixed(2)
+        result.style.display = 'block';
+
+        let resultText = 'Tu IMC es: ' + bmi.toFixed(2);
+
+        //* Determinar clasificación de peso y color resultado
+        let weightClass;
+        let textColor = 'white'; // Default color
 
         //* Tabla del IMC
 
-        if(bmi < 18.5){
-            result.innerHTML += ' - Bajo peso'
-        }else if(bmi < 25){
-            result.innerHTML += ' - Peso normal'
-        }else if(bmi < 30){
-            result.innerHTML += ' - Sobrepeso'
-        }else{
-            result.innerHTML += ' - Obesidad'
+        if (bmi < 18.5) {
+            weightClass = 'Bajo peso';
+            textColor = 'red';
+        } else if (bmi < 25) {
+            weightClass = 'Peso normal';
+            textColor = 'green'; 
+        } else if (bmi < 30) {
+            weightClass = 'Sobrepeso';
+            textColor = 'orange';
+        } else {
+            weightClass = 'Obesidad';
+            textColor = 'red';
         }
+
+        result.innerHTML = resultText + ' - ' + weightClass;
+        result.style.color = textColor; //* Establecer color de texto según el resultado
+
     }
 }
